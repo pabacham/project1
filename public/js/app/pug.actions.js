e2a50f8c62a2559a8d3035b000ae7f345beb2306
Victor Rog <v.rog@itransition.com>
2013-12-17 20:07:20 +0300

    sprite + css fix

--- public/js/app/pug.actions.js
+++ public/js/app/pug.actions.js
@@ -1,42 +1,31 @@
 $(document).ready(function() {
-	// slider
-	$.fn.openSlider = function($slider) {
-        $slider.animate({ right: '0px' }, { duration: 500, specialEasing: { width: 'linear', height: 'easeOutBounce' } });
-    } 
-	$.fn.closeSlider = function($slider) {
-        $slider.animate({ right: '-500px' }, { duration: 500, specialEasing: { width: 'linear', height: 'easeOutBounce' } });
-    } 
-	
-	
-	$(document).on('click', '.open-slider', function(event) {
-		$.fn.openSlider($('#add-tracker'));
-	});
-	$(document).on('click', '.slider-close', function(event) {
-		$.fn.closeSlider($('#add-tracker'));
-	});
-	
-	// tabs
-	$(document).on('click', 'ul#object-detail li a', function(event) {
-		var idContent = $(this).data("linked"),
-			tabContent = $('.tab-on-map');
-		
-		if(tabContent.hasClass('hide-tab-content')) {
-			tabContent.removeClass('hide-tab-content');
-			tabContent.animate({ bottom: '0' }, { duration: 500, specialEasing: { width: 'linear', height: 'easeOutBounce' } });
-		}
-		
-		$("ul#object-detail li").removeClass("selected");
-		$(this).parent().addClass("selected");
-		$('.tab-for-detail').hide();
-		$(idContent).show();
-		return false;
-	});
-	// hide tabs
-	$(document).on('click', 'a.hide-tab', function(event) {
-		var tabContent = $('.tab-on-map'),
-			hTab = tabContent.height() - 45;
-		
-		tabContent.addClass('hide-tab-content');
-		tabContent.animate({ bottom: '-' + hTab + 'px' }, { duration: 500, specialEasing: { width: 'linear', height: 'easeOutBounce' } });
-	});
+
+    $('.open-slider').click(function(){
+        $('.slider-body').toggleClass('open');
+    })
+    $('.slider-close').click(function(){
+        $('.slider-body').toggleClass('open');
+    })
+    $('.selected').click(function(){
+        $(this).closest('.tab-on-map').addClass('open');
+    })
+    $('.hide-tab').click(function(){
+        $(this).closest('.open').removeClass('open');
+    })
+
+    // tabs
+    $(document).on('click', 'ul#object-detail li a', function(event) {
+        var idContent = $(this).data("linked"),
+            tabContent = $('.tab-on-map');
+
+
+
+        $("ul#object-detail li").removeClass("selected");
+        $(this).parent().addClass("selected");
+        $('.tab-for-detail').hide();
+        $(idContent).show();
+        return false;
+    });
+    // hide tabs
+
 }); 
