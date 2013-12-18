389bda3ec2ab81e67e18afc89f06f4a29eb5fa13
Victor Rog <v.rog@itransition.com>
2013-12-18 20:23:13 +0300

    loader

--- public/js/app/pug.actions.js
+++ public/js/app/pug.actions.js
@@ -1,35 +1,32 @@
 $(document).ready(function() {
     var subMenuList = $('.sub-menu li');
-    $('.open-slider').click(function(){
-        $('.slider-body').toggleClass('open');
-    })
-    $('.slider-close').click(function(){
-        $('.slider-body').toggleClass('open');
-    })
-    $('#object-detail li a').click(function(){
-        $(this).closest('.tab-on-map').addClass('open');
-    })
-    $('.hide-tab').click(function(){
-        $(this).closest('.open').removeClass('open');
-    })
-    $(subMenuList).click(function(){
+
+    $(document)
+        .on('click', '#object-detail li a', function() {
+            $(this).closest('.tab-on-map').addClass('open');
+        })
+        .on('click', '.hide-tab', function() {
+            $(this).closest('.open').removeClass('open');
+        })
+        .on('click', '.open-slider', function() {
+            $('.slider-body').toggleClass('open');
+        })
+        .on('click', '.slider-close', function() {
+            $('.slider-body').toggleClass('open');
+        })
+
+        .on('click', 'ul#object-detail li a', function(event) {
+            var idContent = $(this).data("linked"),
+                tabContent = $('.tab-on-map');
+            $("ul#object-detail li").removeClass("selected");
+            $(this).parent().addClass("selected");
+            $('.tab-for-detail').hide();
+            $(idContent).show();
+            return false;
+        })
+    ;
+    $(subMenuList).click(function() {
         $(subMenuList).removeClass('active')
         $(this).addClass('active');
     })
-
-    // tabs
-    $(document).on('click', 'ul#object-detail li a', function(event) {
-        var idContent = $(this).data("linked"),
-            tabContent = $('.tab-on-map');
-
-
-
-        $("ul#object-detail li").removeClass("selected");
-        $(this).parent().addClass("selected");
-        $('.tab-for-detail').hide();
-        $(idContent).show();
-        return false;
-    });
-    // hide tabs
-
 }); 
