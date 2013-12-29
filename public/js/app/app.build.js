0370c538cb615fe591673418ce865a7932d5a5ad
pug-by <a.lomanovich+1@gmail.com>
2013-12-29 15:34:26 +0300

    require js optimizer init

---
+++ public/js/app/app.build.js
@@ -0,0 +1,12 @@
+({
+    baseUrl: ".",
+    paths: {
+        jquery: "libs/jquery/jquery",
+        underscore: 'libs/underscore/underscore',
+        backbone: 'libs/backbone/backbone',
+        async: 'libs/require/async',
+        socketio: '../../socket.io/socket.io'
+    },
+    name: "main",
+    out: "main-built.js"
+})
